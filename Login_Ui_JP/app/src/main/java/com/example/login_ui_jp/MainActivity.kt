613863e8f57b87e9b5a76cbb3f2221382a5c2f08
Prinon bhowmik <prinonbhowmik@Prinons-Mac-mini.local>
2024-01-31 01:08:45 +0600

    login ui

---
+++ Login_Ui_JP/app/src/main/java/com/example/login_ui_jp/MainActivity.kt
@@ -0,0 +1,150 @@
+package com.example.login_ui_jp
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.example.login_ui_jp.ui.theme.Login_Ui_JPTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Login_Ui_JPTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                ) {
+                    Login()
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun Login() {
+    Column(
+        Modifier
+            .padding(22.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(16.dp, alignment = Alignment.Bottom),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            "Welcome",
+            Modifier
+                .fillMaxWidth()
+                .size(30.dp), textAlign = TextAlign.Center
+
+        )
+        TextInput(InputType.Name)
+        TextInput(InputType.Password)
+
+        Button(onClick = {}, modifier = Modifier.fillMaxWidth()) {
+            Text(text = "Sign In", Modifier.padding(vertical = 8.dp))
+        }
+        Divider(
+            color = Color.White.copy(alpha = 0.3f),
+            thickness = 1.5.dp,
+            modifier = Modifier.padding(top = 48.dp)
+        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text("Don't have an account?", color = Color.White)
+            TextButton(onClick = { }) {
+                Text("Sign Up", color = Color.Red)
+            }
+        }
+    }
+}
+
+sealed class InputType(
+    val label: String,
+    val icon: ImageVector,
+    val keyboardOptions: KeyboardOptions,
+    val visualTransformation: VisualTransformation
+) {
+    object Name : InputType(
+        label = "Username",
+        icon = Icons.Default.Person,
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+        visualTransformation = VisualTransformation.None
+    )
+
+    object Password : InputType(
+        label = "Password", icon = Icons.Default.Lock, keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Done, keyboardType = KeyboardType.Password
+        ), visualTransformation = PasswordVisualTransformation()
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TextInput(inputType: InputType) {
+    var value: String by remember { mutableStateOf("") }
+
+    TextField(
+        value = value, 
+        onValueChange = { value = it }, 
+        modifier = Modifier.fillMaxWidth(),
+        leadingIcon = { Icon(imageVector = inputType.icon,
+            null,)},
+        label =  { Text(text = inputType.label)},
+        shape = RoundedCornerShape(12.dp),
+        colors = TextFieldDefaults.textFieldColors(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent,
+            containerColor = Color.White
+        ),
+        singleLine = true,
+        keyboardOptions = inputType.keyboardOptions,
+        visualTransformation = inputType.visualTransformation,
+    )
+}
+
+
